c68c95e405d93359721d201c9988ca8fcdd9dd72
Matheus Hardman <matheus.hardman@gmail.com>
2024-01-13 19:04:29 -0300

    Herança da classe abstrata smartphone nas classes Iphone e Nokia, sobrescrita dos métodos InstalarAplicativo nas classes filhas, testes de execução do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "9998958899", modelo: "OPA11", imei: "178965342", memoria: 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "9999948899", modelo: "KA20", imei: "265478139", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
